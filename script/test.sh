ff7c75b8422fefd0f48d7cc5cb243377090d6bc0
zsq259 <2530059525@qq.com>
2024-12-10 14:20:00 +0000

    fix: test script

--- script/test.sh
+++ script/test.sh
@@ -1,7 +1,10 @@
+test_case="50-1-100"
+output_dir="data/abstask/result/llama-31-8b-instruct-prompt3"
+
 python -m src.main\
     --task abstask\
-    --template "abstask_plan"\
+    --template "abstask_plan_ref"\
     --model "meta-llama/Meta-Llama-3.1-8B-Instruct"\
     --scheduler parallel\
-    --test_file "/home/zhangsq/1/test/data/abstask/dev/30-1-100.json"\
-    --output_file "/home/zhangsq/1/test/data/abstask/result/llama-31-8b-instruct-prompt/30-1-100c-output.json"\
+    --test_file "data/abstask/dev/${test_case}.json"\
+    --output_file "${output_dir}/${test_case}-output.json"\
