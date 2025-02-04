8cd76a6de34d59740ee9fb08e6f024f394721bdb
zsq259 <2530059525@qq.com>
2025-02-04 05:32:35 +0000

    fix: data statistcs

--- src/data_statistics.py
+++ src/data_statistics.py
@@ -1,37 +1,58 @@
 import json
+import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Data statistics")
-parser.add_argument("--file_name", type=str, required=True, default="50-1-100", help="The file name to analyze")
+parser.add_argument("--file_name", type=str, default=None, help="The file name to analyze")
 args = parser.parse_args()
 
-file_path = f"data/dev/{args.file_name}.json"
+def collect_data(file_path):
+    with open(file_path, "r") as f:
+        data = json.load(f)
 
-with open(file_path, "r") as f:
-    data = json.load(f)
+    average_time_ratio = 0
+    average_edge_num = 0
+    average_rule_num = 0
 
-average_time = 0
-average_edge_num = 0
-average_rule_num = 0
-
-for d in data:
-    time_sum = 0
-    average_edge_num += d["edge_count"]
-    average_rule_num += len(d["question"]["rules"])
-    for node in d["answer"]:
-        source = node["source"]
-        target = node["target"]
-        for rule in d["question"]["rules"]:
-            if rule["source"] == source and rule["target"] == target:
-                time_sum += rule["time"]
-                break
-    time_ratio = d["min_time"] / time_sum
-    print(f"Time ratio: {time_ratio:.2f}")
-    average_time += time_ratio
+    for d in data:
+        time_sum = 0
+        average_edge_num += d["edge_count"]
+        average_rule_num += len(d["question"]["rules"])
+        for node in d["answer"]:
+            source = node["source"]
+            target = node["target"]
+            for rule in d["question"]["rules"]:
+                if rule["source"] == source and rule["target"] == target:
+                    time_sum += rule["time"]
+                    break
+        time_ratio = d["min_time"] / time_sum
+        print(f"Time ratio: {time_ratio:.2f}")
+        average_time_ratio += time_ratio
+        
+    average_time_ratio /= len(data)
+    average_edge_num /= len(data)
+    average_rule_num /= len(data)
     
-average_time /= len(data)
-average_edge_num /= len(data)
-average_rule_num /= len(data)
-print(f"Average time ratio: {average_time:.2f}")
-print(f"Average edge number: {average_edge_num:.2f}")
-print(f"Average rule number: {average_rule_num:.2f}")
+    return average_time_ratio, average_edge_num, average_rule_num
+
+
+file_dir = "data/dev/test"
+file_names = []
+if args.file_name:
+    file_names.append(args.file_name)
+else:
+    for file_name in os.listdir(file_dir):
+        if file_name.endswith(".json"):
+            file_names.append(file_name.removesuffix(".json"))            
+    
+# save statistic in a table
+data_statistics = []
+for file_name in file_names:
+    file_path = f"data/dev/test/{file_name}.json"
+    average_time_ratio, average_edge_num, average_rule_num = collect_data(file_path)
+    data_statistics.append([file_name, average_time_ratio])
+    
+data_statistics.sort(key=lambda x: x[0])
+print("File Name\tAverage Time Ratio")
+for d in data_statistics:
+    print(f"{d[0]}\t{d[1]:.2f}")
